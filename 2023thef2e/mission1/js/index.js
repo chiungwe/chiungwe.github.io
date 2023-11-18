4f59bb7752b42693f88e6a54963c70f2d6ce79c4
henry <chiungvi@gmail.com>
2023-11-19 04:07:45 +0800

    切版

---
+++ 2023thef2e/mission1/js/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper("#news_swiper", {
+    slidesPerView: 1.1,
+    spaceBetween: 15,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
